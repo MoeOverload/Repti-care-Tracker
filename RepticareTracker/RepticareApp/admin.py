82c134a882e0913771a0b0558778388967256d54
Morgan Bourne <goombaplayz@gmail.com>
2026-02-06 20:07:01 -0800

    added a lot of models

--- RepticareTracker/RepticareApp/admin.py
+++ RepticareTracker/RepticareApp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Reptile
+from .models import Profile, Reptile, Terrarium, TerrariumLog,FeederType,FeedLog,WeightLog
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Reptile)
-
+admin.site.register(Terrarium)
+admin.site.register(TerrariumLog)
+admin.site.register(FeederType)
+admin.site.register(FeedLog)
+admin.site.register(WeightLog)
--- RepticareTracker/RepticareApp/models.py
+++ RepticareTracker/RepticareApp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
+#user profile model
 class Profile(models.Model):
     user = models.OneToOneField(
         User,
@@ -13,15 +15,101 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
-    
+
+
+#reptile model   
 class Reptile(models.Model):
     owner = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
-        related_name="reptiles"
-        
+        related_name="reptiles"  
     )
     reptile_name = models.CharField("reptile name", max_length=30)
     reptile_species = models.CharField("reptile_species", max_length=60)
+    estimated_age_months = models.IntegerField(null=True, blank=True)
+    estimated_age_record_date = models.DateField(null=True, blank=True)
+
+
     def __str__(self):
         return self.reptile_name
+
+
+#terraium model
+class Terrarium(models.Model):
+    owner = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="terrariums"
+    )
+    reptile = models.OneToOneField(
+        Reptile,
+        on_delete=models.CASCADE,
+        related_name="terrarium"
+    )
+    terrarium_nickname = models.CharField("terrarium_nickname",max_length=30)
+
+
+#food model
+class FeederType(models.Model):
+    name = models.CharField(max_length=60)
+    species = models.CharField(max_length=60, blank=True)
+    default_unit = models.CharField(
+        max_length=10,
+        choices = [
+            ("count", "Count"),
+            ("g","Grams"),
+        ]
+    )
+    notes = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
+#reptile weight log modal
+class WeightLog(models.Model):
+    reptile = models.ForeignKey(
+        Reptile,
+        on_delete=models.CASCADE,
+        related_name="weights"
+    )
+    weight = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    notes = models.TextField(blank = True)
+    recorded_on = models.DateField(auto_now_add=True)
+
+
+#reptile feed log
+class FeedLog(models.Model):
+    reptile = models.ForeignKey(
+        Reptile,
+        on_delete =models.CASCADE,
+        related_name="feedings"
+    )
+    feeder = models.ForeignKey(
+        FeederType,
+        on_delete=models.PROTECT        
+    )
+    quantity = models.DecimalField(
+        max_digits= 6,
+        decimal_places=2
+    )
+    fed_on = models.DateField()
+    notes = models.TextField(blank=True)
+
+
+#terrarium enviroment log
+class TerrariumLog(models.Model):
+    terrarium = models.ForeignKey(
+        Terrarium,
+        on_delete=models.PROTECT,
+        related_name="enviroment_logs"
+    )
+    highest_temp = models.IntegerField(null=True,blank=True)
+    lowest_temp = models.IntegerField(null=True,blank=True)
+    highest_humidity = models.IntegerField(null=True,blank=True)
+    lowest_humidity = models.IntegerField(null=True,blank=True)
+    date_recorded = models.DateField(auto_now_add=True)
+    notes = models.TextField(blank=True)
